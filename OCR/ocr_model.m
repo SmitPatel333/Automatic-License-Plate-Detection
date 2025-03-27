bff4e8b5832ba636c8e794b626c8a96d4a56f0d5
PhotonCrush <tylerlee636@gmail.com>
2025-03-27 19:16:42 -0400

    Neural network for individual characters works

--- OCR/ocr_model.m
+++ OCR/ocr_model.m
@@ -5,9 +5,43 @@ imds = imageDatastore(digitDatasetPath, "IncludeSubfolders", true, "LabelSource"
 
 img = readimage(imds,1);
 
+inputSize = [30 27 1]; % CNN input
+imageAugmenter = imageDataAugmenter( ...
+    'RandXReflection', true, ...
+    'RandXScale', [1 1.2], ...
+    'RandYScale', [1 1.2]);
+
+augimds = augmentedImageDatastore(inputSize, imds, 'DataAugmentation', imageAugmenter);
+
 % Specify the layers and what they contain
 layers = [
     imageInputLayer([30 27 1])
 
     convolution2dLayer(3,8,Padding="same")
+    batchNormalizationLayer
+    reluLayer
+
+    maxPooling2dLayer(2,Stride=2)
+	
+    convolution2dLayer(3,16,Padding="same")
+    batchNormalizationLayer
+    reluLayer
+	
+    maxPooling2dLayer(2,Stride=2)
+	
+    convolution2dLayer(3,32,Padding="same")
+    batchNormalizationLayer
+    reluLayer
+	
+    fullyConnectedLayer(36)
+    softmaxLayer
+    classificationLayer
+
 ];
+
+options = trainingOptions("sgdm", ...
+    InitialLearnRate=0.1, ...
+    MaxEpochs=2, ...
+    Plots="training-progress");    
+
+net = trainNetwork(augimds,layers,options);

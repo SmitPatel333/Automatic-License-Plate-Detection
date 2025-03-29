948c5d7dd6f6cd7078ae945c069d185d7d982948
Smit Patel <Smitypatel03@yahoo.com>
2025-03-29 00:53:58 -0400

    working through list of images that work for the pipeline

--- Stage_1.m
+++ Stage_1.m
@@ -1,17 +1,28 @@
-% I = imread("Dataset\images\Cars24.png");
-I1 = imread("otsu.tif");
+I1 = imread("Dataset\images\Cars11.png");
+% I1 = imread("otsu.tif");
 I = im2gray(I1);
 T = graythresh(I);
 I = imbinarize(I, .5);
 I = edge(I, "canny");
-SE = strel('diamond',9);
+SE = strel('square',5);
 I = imdilate(I, SE);
+I = bwpropfilt(I, "Area", 5, "largest");
 
-I = bwpropfilt(I, "Area", [10000 50000]);
-CC = bwconncomp(I);
-stats = regionprops(CC, "Area", "BoundingBox");
-bbox = stats.BoundingBox;
+% measure perimeter and bounding box for each blob
+stats = regionprops(I,{'Area','BoundingBox','perimeter'});
+stats = struct2table(stats);
+% 1st metric: ratio between perimeter and round length of its bounding box
+stats.Metric1 = 2*sum(stats.BoundingBox(:,3:4),2)./stats.Perimeter;
+idx1 = abs(1 - stats.Metric1) < 0.1;
+% 2nd metric: ratio between blob area and it's bounding box's area
+stats.Metric2 = stats.Area./(stats.BoundingBox(:,3).*stats.BoundingBox(:,4));
+idx2 = stats.Metric2 > 0.8;
+% keep only those which meet both metrics and remove everything else
+idx = idx1 & idx2;
+stats(~idx,:) = [];
 
-new_I = imcrop(I1, bbox);
-
-imshow(new_I);
+% crop the image using the bounding box
+% bbox = stats.BoundingBox;
+% new_I = imcrop(I1, bbox);
+% imshow(new_I);
+imshow(I);

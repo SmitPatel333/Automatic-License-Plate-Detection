06ce8f449308e0d396a48be7c6f6b35c2b172096
PhotonCrush <tylerlee636@gmail.com>
2025-04-07 11:19:58 -0400

    Fixed line with OCR as it was using binImg instead of openImg

--- OCR/plate_reader_2.m
+++ OCR/plate_reader_2.m
@@ -1,7 +1,7 @@
 clc; clear; close all;
 
 % Load an image.
-I = imread("test_plate_7.tif");
+I = imread("test_plate_1.tif");
 grayImg = rgb2gray(I);  % Convert to grayscale (if not already)
 
 threshold = graythresh(grayImg);
@@ -12,7 +12,7 @@ binImg = imclearborder(binImg);  % Removes components with fewer than 50 pixels
 binImg = imcomplement(binImg);
 
 
-se = strel('square', 4);
+se = strel('square', 3);
 
 openImg = imerode(binImg, se);
 figure
@@ -21,6 +21,6 @@ figure
 imshow(openImg);
 
 % Perform OCR.
-results = ocr(binImg, 'LayoutAnalysis', 'line', 'CharacterSet', 'ABCDEFGHIJKLMNOPQRSTUOVWXYZ0123456789');
+results = ocr(openImg, 'LayoutAnalysis', 'line', 'CharacterSet', 'ABCDEFGHIJKLMNOPQRSTUOVWXYZ0123456789');
 
 disp(results.Text);
